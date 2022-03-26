5fdc467c96f3e14ca1c7dac493a654d90f5cd0a5
Alex-Braine <alex.braine.lu@gmail.com>
2022-03-26 21:08:33 +0100

    upd

--- guestfs.cpp
+++ guestfs.cpp
@@ -169,6 +169,6 @@ int main() {
                 }
             }
         }
-        this_thread::sleep_for(chrono::milliseconds(500));
+        this_thread::sleep_for(chrono::milliseconds(1000));
     }
 }
