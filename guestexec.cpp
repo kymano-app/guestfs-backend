8b6e21a56aab4c79bb9f069b9626c246899c606e
Alex-Braine <alex.braine.lu@gmail.com>
2022-04-17 14:29:29 +0200

    upd

--- guestexec.cpp
+++ guestexec.cpp
@@ -47,7 +47,8 @@ void th(int dev_fd, string cmd) {
 
 int main(int argc, char* argv[]) {
     int dev_fd;
-    if ((dev_fd = open(DEV_PATH + argv[1], O_RDWR)) == -1) {
+    string devPath = DEV_PATH + argv[1];
+    if ((dev_fd = open(devPath.c_str(), O_RDWR)) == -1) {
         perror("open");
         exit(1);
     }
